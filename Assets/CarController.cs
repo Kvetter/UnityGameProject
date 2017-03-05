3f7f046d0f2546c943dc018a91ef14d59b98fb4f
Kasper Vetter <Kasper Vetter>
2017-03-05 14:08:19 +0100

    fixed the wheels

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -6,16 +6,40 @@ public class CarController : MonoBehaviour {
 	public List<AxleInfo> axleInfos; // the information about each individual axle
 	public float maxMotorTorque; // maximum torque the motor can apply to wheel
 	public float maxSteeringAngle; // maximum steer angle the wheel can have
+	public GameObject LFW;
+	public GameObject RFW;
+	public GameObject car;
+	float steering;
+
+	public void start(){
+		steering = 0;
+	}
 
 	public void FixedUpdate()
 	{
 		float motor = maxMotorTorque * Input.GetAxis("Vertical");
-		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		if (Input.GetKey ("d")) {
+			if (steering < (car.transform.rotation.y + maxSteeringAngle))
+				steering += Input.GetAxis ("Horizontal");
+			else
+				steering = (car.transform.rotation.y + maxSteeringAngle) - 0.0000000001f;
+		} else if (Input.GetKey ("a")) {
+			if (steering > (car.transform.rotation.y - maxSteeringAngle))
+				steering += Input.GetAxis ("Horizontal");
+			else
+				steering = (car.transform.rotation.y - maxSteeringAngle) + 0.0000000001f;
+		}
+
 
 		foreach (AxleInfo axleInfo in axleInfos) {
 			if (axleInfo.steering) {
 				axleInfo.leftWheel.steerAngle = steering;
 				axleInfo.rightWheel.steerAngle = steering;
+				Vector3 posLeft = LFW.transform.rotation.eulerAngles;
+				LFW.transform.localRotation = Quaternion.Euler(0, axleInfo.leftWheel.steerAngle, 0);
+				Vector3 posRight = RFW.transform.rotation.eulerAngles;
+				RFW.transform.localRotation = Quaternion.Euler(0, axleInfo.rightWheel.steerAngle, 0);
+
 			}
 			if (axleInfo.motor) {
 				axleInfo.leftWheel.motorTorque = motor;
@@ -24,6 +48,7 @@ public class CarController : MonoBehaviour {
 		}
 	}
 }
+
 
 [System.Serializable]
 public class AxleInfo {
