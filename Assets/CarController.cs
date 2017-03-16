93d509300649c60fd8c75ebd3aaa33afcfdaf079
Kasper Vetter <Kasper Vetter>
2017-03-16 11:04:48 +0100

    Car With Physics¨

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -2,22 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CarController : MonoBehaviour {
-	public List<AxleInfo> axleInfos; // the information about each individual axle
-	public float maxMotorTorque; // maximum torque the motor can apply to wheel
-	public float maxSteeringAngle; // maximum steer angle the wheel can have
+public class CarController : MonoBehaviour
+{
+	public List<AxleInfo> axleInfos;
+	// the information about each individual axle
+	public float maxMotorTorque;
+	// maximum torque the motor can apply to wheel
+	public float maxSteeringAngle;
+	// maximum steer angle the wheel can have
 	public GameObject LFW;
 	public GameObject RFW;
 	public GameObject car;
+	Rigidbody rigid;
 	float steering;
+	float hitPoints;
 
-	public void start(){
+	public void Start ()
+	{
 		steering = 0;
+		hitPoints = 100;
+		rigid = car.GetComponent<Rigidbody> ();
 	}
 
-	public void FixedUpdate()
+	public void FixedUpdate ()
 	{
-		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		
+		float motor = maxMotorTorque * Input.GetAxis ("Vertical");
 		if (Input.GetKey ("d")) {
 			if (steering < (car.transform.rotation.y + maxSteeringAngle))
 				steering += Input.GetAxis ("Horizontal");
@@ -36,9 +46,11 @@ public class CarController : MonoBehaviour {
 				axleInfo.leftWheel.steerAngle = steering;
 				axleInfo.rightWheel.steerAngle = steering;
 				Vector3 posLeft = LFW.transform.rotation.eulerAngles;
-				LFW.transform.localRotation = Quaternion.Euler(0, axleInfo.leftWheel.steerAngle, 0);
+				LFW.transform.localRotation = Quaternion.Euler (0, axleInfo.leftWheel.steerAngle, 0);
+				LFW.transform.Rotate (axleInfo.leftWheel.rpm / 60 * 360 * Time.deltaTime, 0, 0);
 				Vector3 posRight = RFW.transform.rotation.eulerAngles;
-				RFW.transform.localRotation = Quaternion.Euler(0, axleInfo.rightWheel.steerAngle, 0);
+				RFW.transform.localRotation = Quaternion.Euler (0, axleInfo.rightWheel.steerAngle, 0);
+				RFW.transform.Rotate (axleInfo.leftWheel.rpm / 60 * 360 * Time.deltaTime, 0, 0);
 
 			}
 			if (axleInfo.motor) {
@@ -47,13 +59,26 @@ public class CarController : MonoBehaviour {
 			}
 		}
 	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.tag == "Wall") {
+			rigid.AddForce (0, 0, 1000000);
+		}
+
+	}
+
 }
 
 
 [System.Serializable]
-public class AxleInfo {
+public class AxleInfo
+{
 	public WheelCollider leftWheel;
 	public WheelCollider rightWheel;
-	public bool motor; // is this wheel attached to motor?
-	public bool steering; // does this wheel apply steer angle?
+	public bool motor;
+	// is this wheel attached to motor?
+	public bool steering;
+	// does this wheel apply steer angle?
 }
+
---
+++ Assets/LFWheel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFWheel : MonoBehaviour {
+
+	public GameObject Wheel;
+	float hitPoints;
+	WheelCollider collider;
+
+	// Use this for initialization
+	void Start () {
+		hitPoints = 100;
+		collider = Wheel.GetComponent<WheelCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onTriggerEnter(Collider other) {
+		Wheel.transform.parent = null;
+	}
+}
